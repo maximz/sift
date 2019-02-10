c6ade9d7c901d523265df2a4e33ac9be33b260aa
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2019-02-10 12:56:32 -0500

    Continue testing work plan functionality.

--- tests/test_status.py
+++ tests/test_status.py
@@ -32,11 +32,12 @@ def mock_index():
             strategy_versions = [1.0 for i in range(len(names))]
         assert len(names) == len(times) == len(
             strategies) == len(strategy_versions)
+        # specify dtypes so that string NaNs become None not np.nan
         return pd.DataFrame({
-            'fname': names,
-            'last_mod': times,
-            'strategy': pd.Series(strategies, dtype='str'), # specify dtype so that NaNs become None not np.nan
-            'strategy_version': strategy_versions,
+            'fname': pd.Series(names, dtype='str'),
+            'last_mod': pd.Series(times, dtype='float'),
+            'strategy': pd.Series(strategies, dtype='str'),
+            'strategy_version': pd.Series(strategy_versions, dtype='float'),
         }).set_index('fname')
     return _mock_index
 
@@ -75,6 +76,26 @@ def test_make_plan_from_scratch(mocker, filetype_strategies, mock_index):
         )
     )
 
+def test_make_plan_from_scratch_empty_directory_dtypes(mocker, filetype_strategies, mock_index):
+    """
+    Confirm make_plan_from_scratch returns the right dtypes when there are no files.
+    """
+    mocker.patch.object(search.status, 'file_list')
+    search.status.file_list.return_value = []
+    assert_frame_equal(
+        search.status.make_plan_from_scratch('.', filetype_strategies),
+        mock_index([], []) # correct dtypes specified here
+    )
+
+
+# expected column order
+diff_plan_expected_column_order = [
+    'last_mod_old', 'strategy_old', 'strategy_version_old', 'last_mod_new',
+    'strategy_new', 'strategy_version_new', '_merge'
+]
+expected_empty_diffplan_shape = (0, len(diff_plan_expected_column_order))
+def empty_diffplan(plan):
+    return plan.shape == expected_empty_diffplan_shape
 
 def test_diff_work_new_files(mock_index):
     """
@@ -102,17 +123,14 @@ def test_diff_work_new_files(mock_index):
                 dtype=pd.api.types.CategoricalDtype(
                     categories=['left_only', 'right_only', 'both'], ordered=False)
             )
-        }).set_index('fname')[[  # expected column order
-            'last_mod_old', 'strategy_old', 'strategy_version_old', 'last_mod_new',
-            'strategy_new', 'strategy_version_new', '_merge'
-        ]]
+        }).set_index('fname')[diff_plan_expected_column_order]
     )
 
-    assert diff_work['unchanged'].shape == (0, 7)
-    assert diff_work['deleted_files'].shape == (0, 7)
-    assert diff_work['updated_files'].shape == (0, 7)
-    assert diff_work['diff_strategy'].shape == (0, 7)
-    assert diff_work['newer_strategy'].shape == (0, 7)
+    assert empty_diffplan(diff_work['unchanged'])
+    assert empty_diffplan(diff_work['deleted_files'])
+    assert empty_diffplan(diff_work['updated_files'])
+    assert empty_diffplan(diff_work['diff_strategy'])
+    assert empty_diffplan(diff_work['newer_strategy'])
 
 
 def test_diff_work_unchanged_file(mock_index):
@@ -124,11 +142,11 @@ def test_diff_work_unchanged_file(mock_index):
 
     diff_work = search.status.diff_work_between_plans(index, index)
 
-    assert diff_work['new_files'].shape == (0, 7)
-    assert diff_work['deleted_files'].shape == (0, 7)
-    assert diff_work['updated_files'].shape == (0, 7)
-    assert diff_work['diff_strategy'].shape == (0, 7)
-    assert diff_work['newer_strategy'].shape == (0, 7)
+    assert empty_diffplan(diff_work['new_files'])
+    assert empty_diffplan(diff_work['deleted_files'])
+    assert empty_diffplan(diff_work['updated_files'])
+    assert empty_diffplan(diff_work['diff_strategy'])
+    assert empty_diffplan(diff_work['newer_strategy'])
 
     assert_frame_equal(
         diff_work['unchanged'],
@@ -145,10 +163,7 @@ def test_diff_work_unchanged_file(mock_index):
                 dtype=pd.api.types.CategoricalDtype(
                     categories=['left_only', 'right_only', 'both'], ordered=False)
             )
-        }).set_index('fname')[[  # expected column order
-            'last_mod_old', 'strategy_old', 'strategy_version_old', 'last_mod_new',
-            'strategy_new', 'strategy_version_new', '_merge'
-        ]]
+        }).set_index('fname')[diff_plan_expected_column_order]
     )
 
 
@@ -177,16 +192,13 @@ def test_diff_work_new_and_unchanged_files(mock_index):
                 dtype=pd.api.types.CategoricalDtype(
                     categories=['left_only', 'right_only', 'both'], ordered=False)
             )
-        }).set_index('fname')[[ # expected column order
-            'last_mod_old', 'strategy_old', 'strategy_version_old', 'last_mod_new',
-            'strategy_new', 'strategy_version_new', '_merge'
-        ]]
+        }).set_index('fname')[diff_plan_expected_column_order]
     )
 
-    assert diff_work['deleted_files'].shape == (0, 7)
-    assert diff_work['updated_files'].shape == (0, 7)
-    assert diff_work['diff_strategy'].shape == (0, 7)
-    assert diff_work['newer_strategy'].shape == (0, 7)
+    assert empty_diffplan(diff_work['deleted_files'])
+    assert empty_diffplan(diff_work['updated_files'])
+    assert empty_diffplan(diff_work['diff_strategy'])
+    assert empty_diffplan(diff_work['newer_strategy'])
 
     assert_frame_equal(
         diff_work['unchanged'],
@@ -203,16 +215,51 @@ def test_diff_work_new_and_unchanged_files(mock_index):
                 dtype=pd.api.types.CategoricalDtype(
                     categories=['left_only', 'right_only', 'both'], ordered=False)
             )
-        }).set_index('fname')[[  # expected column order
-            'last_mod_old', 'strategy_old', 'strategy_version_old', 'last_mod_new',
-            'strategy_new', 'strategy_version_new', '_merge'
-        ]]
+        }).set_index('fname')[diff_plan_expected_column_order]
     )
 
 
-def test_diff_work_removed_files(mock_index):
+
+
+
+def test_diff_work_removed_only_file(mock_index):
     """
-    Confirm diff_work_between_plans produces proper classifications on synthetic plans, in this case with a removed file.
+    Confirm diff_work_between_plans produces proper classifications on synthetic plans, in this case with 1 removed file.
+    """
+    common_time = time.time()
+    old_index = mock_index(['file.txt'], [common_time])
+    new_index = mock_index([], [])
+
+    diff_work = search.status.diff_work_between_plans(old_index, new_index)
+
+    assert_frame_equal(
+        diff_work['deleted_files'],
+        pd.DataFrame({
+            'fname': ['file.txt'],
+            'last_mod_old': [common_time],
+            'last_mod_new': [np.nan],  # float
+            'strategy_new': [None],  # object
+            'strategy_old': ['MyImporter'],
+            'strategy_version_new': [np.nan],  # float
+            'strategy_version_old': [1.0],
+            '_merge': pd.Series(
+                ['left_only'],
+                dtype=pd.api.types.CategoricalDtype(
+                    categories=['left_only', 'right_only', 'both'], ordered=False)
+            )
+        }).set_index('fname')[diff_plan_expected_column_order]
+    )
+
+    assert empty_diffplan(diff_work['new_files'])
+    assert empty_diffplan(diff_work['updated_files'])
+    assert empty_diffplan(diff_work['diff_strategy'])
+    assert empty_diffplan(diff_work['newer_strategy'])
+    assert empty_diffplan(diff_work['unchanged'])
+
+
+def test_diff_work_removed_and_unchanged_files(mock_index):
+    """
+    Confirm diff_work_between_plans produces proper classifications on synthetic plans, in this case with 1 removed, 1 unchanged file.
     """
     common_time = time.time()
     old_index = mock_index(['permanent_file.txt', 'temporary_file.txt'], [
@@ -236,16 +283,13 @@ def test_diff_work_removed_files(mock_index):
                 dtype=pd.api.types.CategoricalDtype(
                     categories=['left_only', 'right_only', 'both'], ordered=False)
             )
-        }).set_index('fname')[[  # expected column order
-            'last_mod_old', 'strategy_old', 'strategy_version_old', 'last_mod_new',
-            'strategy_new', 'strategy_version_new', '_merge'
-        ]]
+        }).set_index('fname')[diff_plan_expected_column_order]
     )
 
-    assert diff_work['new_files'].shape == (0, 7)
-    assert diff_work['updated_files'].shape == (0, 7)
-    assert diff_work['diff_strategy'].shape == (0, 7)
-    assert diff_work['newer_strategy'].shape == (0, 7)
+    assert empty_diffplan(diff_work['new_files'])
+    assert empty_diffplan(diff_work['updated_files'])
+    assert empty_diffplan(diff_work['diff_strategy'])
+    assert empty_diffplan(diff_work['newer_strategy'])
 
     assert_frame_equal(
         diff_work['unchanged'],
@@ -262,67 +306,42 @@ def test_diff_work_removed_files(mock_index):
                 dtype=pd.api.types.CategoricalDtype(
                     categories=['left_only', 'right_only', 'both'], ordered=False)
             )
-        }).set_index('fname')[[  # expected column order
-            'last_mod_old', 'strategy_old', 'strategy_version_old', 'last_mod_new',
-            'strategy_new', 'strategy_version_new', '_merge'
-        ]]
+        }).set_index('fname')[diff_plan_expected_column_order]
     )
-
 
 def test_diff_work_updated_files(mock_index):
     """
-    Confirm diff_work_between_plans produces proper classifications on synthetic plans, in this case with a removed file.
+    Confirm diff_work_between_plans produces proper classifications on synthetic plans, in this case with an updated file.
     """
     common_time = time.time()
-    old_index = mock_index(['permanent_file.txt', 'temporary_file.txt'], [
-                           common_time, common_time])
-    new_index = mock_index(['permanent_file.txt'], [common_time])
+    old_index = mock_index(['file.txt'], [common_time - 1000])
+    new_index = mock_index(['file.txt'], [common_time])
 
     diff_work = search.status.diff_work_between_plans(old_index, new_index)
 
     assert_frame_equal(
-        diff_work['deleted_files'],
+        diff_work['updated_files'],
         pd.DataFrame({
-            'fname': ['temporary_file.txt'],
-            'last_mod_old': [common_time],
-            'last_mod_new': [np.nan],  # float
-            'strategy_new': [None],  # object
-            'strategy_old': ['MyImporter'],
-            'strategy_version_new': [np.nan],  # float
-            'strategy_version_old': [1.0],
-            '_merge': pd.Series(
-                ['left_only'],
-                dtype=pd.api.types.CategoricalDtype(
-                    categories=['left_only', 'right_only', 'both'], ordered=False)
-            )
-        }).set_index('fname')[[  # expected column order
-            'last_mod_old', 'strategy_old', 'strategy_version_old', 'last_mod_new',
-            'strategy_new', 'strategy_version_new', '_merge'
-        ]]
-    )
-
-    assert diff_work['new_files'].shape == (0, 7)
-    assert diff_work['updated_files'].shape == (0, 7)
-    assert diff_work['diff_strategy'].shape == (0, 7)
-    assert diff_work['newer_strategy'].shape == (0, 7)
-
-    assert_frame_equal(
-        diff_work['unchanged'],
-        pd.DataFrame({
-            'fname': ['permanent_file.txt'],
-            'last_mod_old': [common_time],
+            'fname': ['file.txt'],
+            'last_mod_old': [common_time - 1000],
             'last_mod_new': [common_time],
-            'strategy_old': ['MyImporter'],
             'strategy_new': ['MyImporter'],
-            'strategy_version_old': [1.0],
+            'strategy_old': ['MyImporter'],
             'strategy_version_new': [1.0],
+            'strategy_version_old': [1.0],
             '_merge': pd.Series(
                 ['both'],
                 dtype=pd.api.types.CategoricalDtype(
                     categories=['left_only', 'right_only', 'both'], ordered=False)
             )
-        }).set_index('fname')[[  # expected column order
-            'last_mod_old', 'strategy_old', 'strategy_version_old', 'last_mod_new',
-            'strategy_new', 'strategy_version_new', '_merge'
-        ]]
+        }).set_index('fname')[diff_plan_expected_column_order]
     )
+
+    assert empty_diffplan(diff_work['new_files'])
+    assert empty_diffplan(diff_work['deleted_files'])
+    assert empty_diffplan(diff_work['diff_strategy'])
+    assert empty_diffplan(diff_work['newer_strategy'])
+    assert empty_diffplan(diff_work['unchanged'])
+
+
+# TODO: test diff strategy, newer strategy
